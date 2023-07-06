631487d516692381fcdf741193920a67ebbf719b
MBonalumi <10607992@polimi.it>
2023-07-06 10:46:17 +0200

    added computation usind D^{-1} and its schur complement instead of A^{-1} and its schur complement

--- matrix_block_inversion.py
+++ matrix_block_inversion.py
@@ -1,27 +1,30 @@
 import numpy as np
 
+
 def matrix_block_inversion(Ainv,B,C,D):
     '''
-    NB: the inverse of a matrix M is here indicated as M'
-
-    we perform the inverse of a matrix
+    This function performs the inverted of a matrix X, defined by blocks as:
     
-    X = [A B]
-        [C D]
+            X = [A B]
+                [C D]
     
-    by using Block Matrix Inversion.
-    The algorithm requires A and its schur complement D-CA'B to be invertible.
-    If not, one could use D and its schur complement A-BD'C instead, being sure those are invertible.
+    The algorithm performed is Block Matrix Inversion.
+    (refer to https://arxiv.org/pdf/2305.11103.pdf for more information)
 
-    If A and its Schur complement S are invertible, then the inverse of X is
-    (see https://arxiv.org/pdf/2305.11103.pdf)
+    This algorithm requires A and its schur complement S(A) = D-CA'B to be invertible.
+    If not, one could use D and its schur complement S(D) = A-BD'C instead, being sure those are invertible.
+    In this case call matrix_block_inversion_D instead.
 
-    X'= [  A' + A'BS'CA'   -A'BS'  ]
-        [     -S'CA'         S'    ]
+    If A and its Schur complement S are invertible, then the inverse of X is:
 
-    where only the inverse of A and S are required.
+            X'= [  A' + A'BS'CA'   -A'BS'  ]
+                [     -S'CA'         S'    ]
+    
+    The advantage is that only the inverse of A and S are required.
+    This come in handy when A' is already known.
 
-    This come in handy since A' is already known in this application and S' in very much smaller than X'.
+    For example when expanding the kernel matrix in Gaussian Processes Tenciques.
+    (see https://github.com/MBonalumi/Batch-Incremental-Gaussian-Process-Regression-BIGPR)
     '''
 
     assert Ainv.shape[0] == Ainv.shape[1]
@@ -37,6 +40,52 @@ def matrix_block_inversion(Ainv,B,C,D):
     Xinv01 = -Ainv @ B @ Sinv
     Xinv10 = -Sinv @ C @ Ainv
     Xinv11 = Sinv
+
+    Xinv = np.block([   [Xinv00, Xinv01],
+                        [Xinv10, Xinv11]   ])
+
+    return Xinv
+
+
+def matrix_block_inversion_D(A,B,C,Dinv):
+    '''
+    This function performs the inverted of a matrix X, defined by blocks as:
+    
+            X = [A B]
+                [C D]
+    
+    The algorithm performed is Block Matrix Inversion.
+    (refer to https://arxiv.org/pdf/2305.11103.pdf for more information)
+
+    This algorithm requires D and its schur complement S(D) = A-BD'C to be invertible.
+    If not, one could use A and its schur complement S(A) = D-CA'B instead, being sure those are invertible.
+    In this case call matrix_block_inversion instead.
+
+    If D and its Schur complement S are invertible, then the inverse of X is:
+
+            X'= [      S'        -S'BD'     ]
+                [   -D'CS'     D'+D'CS'BD'  ]
+    
+    The advantage is that only the inverse of A and S are required.
+    This come in handy when A' is already known.
+
+    For example when expanding the kernel matrix in Gaussian Processes Tenciques.
+    (see https://github.com/MBonalumi/Batch-Incremental-Gaussian-Process-Regression-BIGPR)
+    '''
+
+    assert A.shape[0] == A.shape[1]
+    assert Dinv.shape[0] == Dinv.shape[1]
+    assert B.shape[0] == A.shape[0] and B.shape[1] == Dinv.shape[0]
+    assert C.shape[0] == Dinv.shape[0] and C.shape[1] == A.shape[0]
+
+
+    S = A - B @ Dinv @ C
+    Sinv = np.linalg.inv(S)
+
+    Xinv00 = Sinv
+    Xinv01 = -Sinv @ B @ Dinv
+    Xinv10 = -Dinv @ C @ Sinv
+    Xinv11 = Dinv + Dinv @ C @ Sinv @ B @ Dinv
 
     Xinv = np.block([   [Xinv00, Xinv01],
                         [Xinv10, Xinv11]   ])
